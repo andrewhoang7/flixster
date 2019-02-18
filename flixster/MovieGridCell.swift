1fb86d990f5d74040b653392a9435c829acd6476
Andrew Hoang <andrew@wireless-10-146-234-218.public.utexas.edu>
2019-02-18 17:25:50 -0600

    implemented navigation to flixter app

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Andrew Hoang on 2/18/19.
+//  Copyright © 2019 andrewhoang7@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+
+}
